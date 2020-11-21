2ee8755605a4bcb35426ef92e7e39714137afa8e
Lucas Rocha <ld.rocha@hotmail.com>
2020-11-20 17:09:21 -0800

    completed exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,24 +1,35 @@
-function add () {
-	
+function add (n1, n2) {
+	return n1 + n2;
 }
 
-function subtract () {
-	
+function subtract (n1, n2) {
+	return n1 - n2;
 }
 
-function sum () {
-	
+function sum (numbers) {
+	return numbers.reduce((total, amount) => total + amount, 0);
 }
 
-function multiply () {
-	
+function multiply (numbers) {
+	return numbers.reduce((total, amount) => total * amount);
 }
 
-function power() {
-	
+function power(num, pow) {
+	let total = num
+	for (let i = 1; i < pow; i++) {
+		total *= num;
+	}
+
+	return total;
 }
 
-function factorial() {
+function factorial(num) {
+	let total = 1;
+	for (let i = num; i >= 1; i--) {
+		total *= i
+	}
+
+	return total;
 	
 }
 
