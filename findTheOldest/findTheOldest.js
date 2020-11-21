78bde357b9edbc863622f23f1a470ee8f09861ab
Lucas Rocha <ld.rocha@hotmail.com>
2020-11-20 20:43:34 -0800

    completed exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    const oldest = people.reduce((oldestPerson, current) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
 
+        return oldestAge < currentAge ? current : oldestPerson;
+    });
+
+    return oldest;
+};
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+
+    return death - birth;
 }
 
 module.exports = findTheOldest
